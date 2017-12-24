cf549f2dfa06f516e9565a944ffe4cc0d293cac1
simonk <jackofspaces@gmail.com>
2017-12-24 13:35:27 +0000

    Adding orders to the cucumber world

--- java-framework-simonkay/src/test/java/com/simonkay/javaframework/configurations/CucumberWorld.java
+++ java-framework-simonkay/src/test/java/com/simonkay/javaframework/configurations/CucumberWorld.java
@@ -1,12 +1,29 @@
 package com.simonkay.javaframework.configurations;
 
 import java.util.HashMap;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import datamodels.Order;
 
 public class CucumberWorld {
 	private static final Logger LOG = LogManager.getLogger(CucumberWorld.class);
 	
 	public HashMap<String, String> stepDefinitionState = new HashMap<String, String>();
+	private HashMap<String, Order> orders = new HashMap<String, Order>();
+	
+	
+	public HashMap<String, Order> getOrdersMap() {
+		return orders;
+	}
+	
+	public void addNewOrder(String orderAlias, Order order) {
+		orders.put(orderAlias, order);
+	}
+	
+	public Order getOrderByAlias(String orderAlias) {
+		return orders.get(orderAlias);
+	}
 	
 }
