3f3936c5003813bd6ba4519238a2f4ed5fba6a9d
simonk <jackofspaces@gmail.com>
2018-01-07 11:51:51 +0000

    removed unused state in world

--- src/test/java/com/simonkay/javaframework/configurations/CucumberWorld.java
+++ src/test/java/com/simonkay/javaframework/configurations/CucumberWorld.java
@@ -9,25 +9,24 @@ import com.simonkay.javaframework.datamodels.Order;
 
 public class CucumberWorld {
 	private static final Logger LOG = LogManager.getLogger(CucumberWorld.class);
-	
-	public HashMap<String, String> stepDefinitionState = new HashMap<String, String>();
-	private HashMap<String, Object> orders = new HashMap<String, Object>();
+
+	private HashMap<String, Object> state = new HashMap<String, Object>();
 	
 	
 	public HashMap<String, Object> getOrdersMap() {
-		return orders;
+		return state;
 	}
 	
 	public void addNewOrder(String orderAlias, Order order) {
 		LOG.debug("Adding new order to cucumber world with alias: " + orderAlias + ". Order: " + order.toString());
-		orders.put(orderAlias, order);
+		state.put(orderAlias, order);
 	}
 	
 	public Order getOrderByAlias(String orderAlias) {
 		LOG.debug("Retrieving order from cucumber world with alias: " + orderAlias);
-		if (orders.get(orderAlias) instanceof Order) {
+		if (state.get(orderAlias) instanceof Order) {
 			LOG.debug("Alias is infact an Order, casting from Obj to Order");
-			return (Order) orders.get(orderAlias);
+			return (Order) state.get(orderAlias);
 		}
 		LOG.fatal("ATTEMPTING TO RETRIEVE AN INCORRECT OBJECT TYPE FROM THE WORLD, IT WAS NOT AN INSTANCE OF ORDER, RETURNING NULL");
 		return null;		
